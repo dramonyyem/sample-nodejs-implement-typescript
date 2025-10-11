f556f3a10446b9806f40fff6ae3b7df1c4282912
daramonyyem <yem.daramony@gmail.com>
2025-10-11 21:58:06 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express , { Request, Response } from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to the TypeScript API!");
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
